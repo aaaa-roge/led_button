5cda221d2dfb77d95c254b2acd252ced69f3d1ac
aaaa-roge <roged@VENGEANCE>
2023-09-17 22:35:44 -0700

    cleanup

--- ProjectButton/main.c
+++ ProjectButton/main.c
@@ -13,7 +13,9 @@
 static SemaphoreHandle_t mutex;
 
 /* declare LED pin numbers, this could technically be variable?
-   decided not to so that we know where LEDs are wired up  */
+   decided not to so that we know where LEDs are wired up  
+   LED_PINS will be in order from leftmost LED being at the start
+   of the array and the rightmost LED being at the end */
 const uint LED_PINS[LED_ARRAY_LENGTH] = {22, 21, 20, 19, 18};
 
 // set bounds for moving the LED
@@ -31,24 +33,30 @@ void led_task() {
     while(1) {
 
         if(gpio_get(LEFT_BUTTON_PIN) && currentPinIndex > 0 && buttonOpen) {
-             buttonOpen = false;
+
+            buttonOpen = false;
             gpio_put(LED_PINS[currentPinIndex], 0);
             printf("L previous pin/index = %d / %d\n", currentPinIndex, LED_PINS[currentPinIndex]);
+            // move to the left
             currentPinIndex--;
             printf("L new pin/index = %d / %d\n", currentPinIndex, LED_PINS[currentPinIndex]);
             gpio_put(LED_PINS[currentPinIndex], 1);
-        } 
-        else if(gpio_get(RIGHT_BUTTON_PIN) && currentPinIndex < LED_ARRAY_LENGTH-1 && buttonOpen) {
-             buttonOpen = false;
+
+        }  else if(gpio_get(RIGHT_BUTTON_PIN) && currentPinIndex < LED_ARRAY_LENGTH-1 && buttonOpen) {
+
+            buttonOpen = false;
             gpio_put(LED_PINS[currentPinIndex], 0);
             printf("R previous pin/index = %d / %d\n", currentPinIndex, LED_PINS[currentPinIndex]);
+            // move to the right
             currentPinIndex++;
             printf("R new pin/index = %d / %d\n", currentPinIndex, LED_PINS[currentPinIndex]);
             gpio_put(LED_PINS[currentPinIndex], 1);
-        } 
-        else if(!gpio_get(LEFT_BUTTON_PIN) && !gpio_get(RIGHT_BUTTON_PIN)) {
+
+        } else if(!gpio_get(LEFT_BUTTON_PIN) && !gpio_get(RIGHT_BUTTON_PIN)) {
+
             buttonOpen = true;
             printf("current pin/index = %d / %d\n", currentPinIndex, LED_PINS[currentPinIndex]);
+            
         }
     }
 }
---
+++ readme.md
@@ -0,0 +1,10 @@
+Project is "ProjectButton"
+
+setup:
+
+button - LED - LED - LED - LED - LED - button
+
+center LED is on
+press left or right buttons to make the light move and stuff
+
+work in progress
